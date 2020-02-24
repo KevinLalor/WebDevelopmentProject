53517eebac2f03219457a3615e8e475692374e60
Liam Phelan <liamphelan2@gmail.com>
2020-02-24 15:04:16 +0000

    setup spring boot with maven

---
+++ src/main/java/ie/ucd/lblms/LbLmsApplication.java
@@ -0,0 +1,13 @@
+package ie.ucd.lblms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbLmsApplication.class, args);
+	}
+
+}
