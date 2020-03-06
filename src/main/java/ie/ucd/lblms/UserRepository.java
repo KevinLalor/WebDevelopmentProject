224ba955d903b99d48310db8e9a894ec37f555ff
unknown <oscar.byrnecarty@ucdconnect.ie>
2020-03-06 14:58:59 +0000

    added credentials class for authentication process

--- src/main/java/ie/ucd/lblms/UserRepository.java
+++ src/main/java/ie/ucd/lblms/UserRepository.java
@@ -1,8 +1,15 @@
 package ie.ucd.lblms;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserById(long id);
+    public Optional<User> findUsernameAndPassword(long id);
+    // Add query to find username and password here
+    public List<Credentials> getCredentials(@Param("user") String username, @Param("password") String password);
 }
