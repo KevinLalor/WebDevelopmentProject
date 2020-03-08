c1ba522227260674956b57eb4adc0911cde29d7a
unknown <oscar.byrnecarty@ucdconnect.ie>
2020-03-08 15:10:33 +0000

    added catalogue of members (unfinished)

--- src/main/java/ie/ucd/lblms/UserRepository.java
+++ src/main/java/ie/ucd/lblms/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>
-{ 
+{
     Optional<User> findByUsernameAndPassword(String username, String password);
+    User findById(long id);
 }
