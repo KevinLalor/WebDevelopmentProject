f50eff8950f315ed35be18ba158e51f280ed7408
Liam Phelan <liamphelan2@gmail.com>
2020-03-08 17:10:35 +0000

    removed extraneous dependency

--- src/main/java/ie/ucd/lblms/LibraryController.java
+++ src/main/java/ie/ucd/lblms/LibraryController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.ui.Model;
 
 import java.util.List;
