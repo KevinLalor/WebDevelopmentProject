ecff3bf6ddb637df253b8f7e6256401037fb0e8c
Liam Phelan <liamphelan2@gmail.com>
2020-03-21 20:11:18 +0000

    removed unused dependencies

--- src/main/java/ie/ucd/lblms/LibraryController.java
+++ src/main/java/ie/ucd/lblms/LibraryController.java
@@ -1,7 +1,6 @@
 package ie.ucd.lblms;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
